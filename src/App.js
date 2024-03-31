6aea8e82e972d3fbc4a2ecbdeda249d729a41ff1
Valère Dunand <valere.d@gmail.com>
2024-04-01 00:18:17 +0200

    Extract tree grow into own script

--- src/App.js
+++ src/App.js
@@ -3,20 +3,13 @@ import TreeRender from './renderer/TreeRender.js'
 import LightRender from './renderer/LightRender.js'
 import LightDirectional from './LightDirectional.js';
 import {Tree} from './Tree.js';
-import RBush from '../vendor/rbush.js';
-import Attractor from './Attractor.js';
-import RBushKnn from '../vendor/rbush-knn.js';
-import {presets} from './Tree.js';
+import { presets } from './Tree.js';
+import { treeGrowUpdate } from './TreesUpdater.js';
 import * as UiControls from './UiControls.js';
 import * as UiMouse from './UiMouse.js';
 import UiCut from './UiCut.js';
 import * as ImageLoader from './ImageLoader.js';
 
-const rbushAttractors = new RBush();
-const rbushBranchs = new RBush();
-
-const illuminatedBranchs = new Set();
-let attractors = [];
 
 const treesList = [];
 const treesSolo = [];
@@ -140,13 +133,6 @@ function onKeyUp(evt) {
     }
 }
 
-function testDrawLeaves() {
-    for (const trees of treesList) {
-        trees.forEach(tree => treeRender.testLeaves(tree));
-    }
-    treeLayer.drawIntoContext(context);
-}
-
 function onMouseDown() {
     run = true;
 }
@@ -177,9 +163,15 @@ function updateScreen() {
     lightLayer.drawIntoContext(context);
 }
 
+function testDrawLeaves() {
+    for (const trees of treesList) {
+        trees.forEach(tree => treeRender.testLeaves(tree));
+    }
+}
+
 function updateTrees() {
     for (const trees of treesList) {
-        treeGrow(trees);
+        treeGrowUpdate(trees, lightSource, applyBend);
     }
     
     drawTrees();
@@ -192,110 +184,10 @@ function drawTrees() {
     }
 }
 
-function treeGrow(trees) {
-    rbushAttractors.clear();
-    rbushBranchs.clear();
-
-    const lightPosition = canvasToWorldPosition(new Vector(UiMouse.mousePosition[0], UiMouse.mousePosition[1]));
-    lightSource.reset(new Vector(lightPosition[0], lightPosition[1]));
-
-    const branchs = [];
-    trees.forEach(tree => branchs.push(...tree.getBranchs()));
-    indexBranchs(branchs);
-    UiCut.setBranches(rbushBranchs);
-    lightSource.emit(rbushBranchs);
-
-    attractors = createAttractors(lightSource.getPhotons());
-
-    branchs.forEach(branch => branch.clearAttractors());
-    
-    branchs.forEach(branch => attachBranchToAttractors(branch));
-    attachAttractorsToBranch(attractors);
-    
-    trees.forEach(tree => tree.startCycle());
-    branchs.forEach(branch => branch.startCycle());
-
-    for (let branch of illuminatedBranchs) {
-        branch.takeLight();
-    }
-
-    trees.forEach(tree => tree.distributeEnergy());
-    trees.forEach(tree => tree.prune());
-    if (applyBend) {
-        trees.forEach(tree => tree.bendBranches());
-    }
-
-    trees.forEach(tree => tree.endCycle());
-}
-
-function indexBranchs(branchs) {
-    const items = [];
-
-    for (let i = 0; i < branchs.length; i ++) {
-        const branch = branchs[i];
-
-        const branchBbox = {
-            minX: Math.min(branch.start.x, branch.end.x),
-            maxX: Math.max(branch.start.x, branch.end.x),
-            minY: Math.min(branch.start.y, branch.end.y),
-            maxY: Math.max(branch.start.y, branch.end.y),
-            branch: branch,
-        };
-
-        items.push(branchBbox);
-    }
-    
-    rbushBranchs.load(items);
-}
-
-function attachBranchToAttractors(branch) {
-    const nearAttractors = RBushKnn(rbushAttractors, branch.end.x, branch.end.y, undefined, undefined, branch.maxLightDistance);
-    nearAttractors.forEach(attractorItem => attractorItem.attractor.attachBranchIfNeeded(branch));
-}
-
-function attachAttractorsToBranch(attractors) {
-
-    illuminatedBranchs.clear();
-
-    for (let i = 0; i < attractors.length; i ++) {
-        const attractor = attractors[i];
-        const branch = attractor.nearestBranch;
-        if (branch === null) {
-            continue;
-        }
-        branch.attractors.push(attractor);
-        illuminatedBranchs.add(branch);
-    }
-}
-
-function createAttractors(photons) {
-
-    const attractors = [];
-    const items = [];
-
-    for (let i = 0; i < photons.length; i ++) {
-        const photon = photons[i];
-        const attractor = new Attractor(photon.position, photon.orientation);
-        attractors.push(attractor);
-
-        items.push({
-            minX: photon.position.x,
-            maxX: photon.position.x,
-            minY: photon.position.y,
-            maxY: photon.position.y,
-            attractor: attractor,
-        });
-    }
-    
-    rbushAttractors.load(items);
-
-
-    return attractors;
-}
-
 function onMouseWheel(evt) {
     changeScale(evt.deltaY);
     drawTrees();
+    testDrawLeaves();
 }
 
 function clearCanvas() {
